5624c9912999effd504235835ff63acf5eca6bac
Azeem Rajput <muhammadaxeem24@gmail.com>
2025-09-26 04:20:46 +0500

    added initial code

---
+++ TrailingZeroInfactorial.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int counter(int n)
+{
+    int count=0;
+    while(n>=5)
+    {
+        count+=n/5;
+        n/=5;
+    }
+    return count;
+  
+}
+int main()
+{
+    int n;
+    cout << "Enter a number=";
+    cin >> n;
+    cout << counter(n);
+}
